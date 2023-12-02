5577bba9e73359547305785cfde8d45ba3904c51
Rashid-qa <131335264+Rashid-qa@users.noreply.github.com>
2023-12-03 03:02:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
+# app.py
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is your web server.'
+    return 'Hello, this is your web server!'
 
 if __name__ == '__main__':
-    app.run('0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
+
